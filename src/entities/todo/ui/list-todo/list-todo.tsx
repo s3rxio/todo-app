be3d7d660b0b01b74759b13d5b77c97a6825bfe6
s3rxio <cergotabagua222@gmail.com>
2024-10-21 07:05:03 +0300

    fix(todo): fix todos rerenders

--- src/entities/todo/ui/list-todo/list-todo.tsx
+++ src/entities/todo/ui/list-todo/list-todo.tsx
@@ -6,11 +6,29 @@ interface ListTodoProps {
   todos: TodoModel[];
   render: (todo: TodoModel, i: number) => JSX.Element;
   reverse?: boolean;
+  onlyActive?: boolean;
+  onlyCompleted?: boolean;
 }
-export const ListTodo: FC<ListTodoProps> = ({ todos, render, reverse }) => {
+export const ListTodo: FC<ListTodoProps> = ({
+  todos,
+  render,
+  reverse,
+  onlyActive,
+  onlyCompleted,
+}) => {
+  let handledTodos = [...todos];
+
+  if (onlyActive) {
+    handledTodos = todos.filter((todo) => !todo.completed);
+  } else if (onlyCompleted) {
+    handledTodos = todos.filter((todo) => todo.completed);
+  }
+  if (reverse) {
+    handledTodos = handledTodos.reverse();
+  }
   return (
     <div className={styles.listTodo}>
-      {(reverse ? todos.reverse() : todos).map((todo, i) => render(todo, i))}
+      {handledTodos.map((todo, i) => render(todo, i))}
     </div>
   );
 };
--- src/widget/todos/ui/todos/todos.tsx
+++ src/widget/todos/ui/todos/todos.tsx
@@ -4,13 +4,20 @@ import styles from "./todos.module.scss";
 export const Todos = () => {
   const { todos } = useTodos();
 
+  const duplicatedTodos = todos.filter(
+    (todo, i) => todos.findIndex((t) => t.id === todo.id) !== i
+  );
+
+  console.log(duplicatedTodos);
+
   return (
     <div className={styles.todos}>
       <AddTodo className={styles.todos__addTodo} />
       <ListTodo
-        todos={todos.reverse()}
+        todos={todos}
+        reverse
         render={(todo, i) => (
-          <Todo key={todo.id + i} todo={todo} className={styles.todos__todo} />
+          <Todo key={i} todo={todo} className={styles.todos__todo} />
         )}
       />
     </div>
